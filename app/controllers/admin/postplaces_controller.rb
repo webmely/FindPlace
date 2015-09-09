0057538fd00b9f971bcb97b5adce1abbef494f68
Le Duc Son <ducson.aptech@gmail.com>
2015-09-09 22:55:57 +0700

    ckeditor for post

--- app/controllers/admin/postplaces_controller.rb
+++ app/controllers/admin/postplaces_controller.rb
@@ -12,12 +12,25 @@ module Admin
 			@postplace = Postplace.new
 		end
 
+		def edit
+			@postplace = Postplace.find(params[:id])
+		end
+
 		def create
 			@postplace = Postplace.new(post_params)
 			if @postplace.save
 				redirect_to admin_postplaces_path
 			else
 				render 'new'
+			end
+		end
+
+		def update
+			@postplace = Postplace.find(params[:id])
+			if @postplace.update(post_params)
+				redirect_to admin_postplaces_path
+			else
+				render 'edit'
 			end
 		end
 
